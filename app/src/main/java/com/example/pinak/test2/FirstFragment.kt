88e85106092a1d32a3a9b8a72ebfc706ca8990a3
pinakim11 <pinakim11@gmail.com>
2017-06-26 17:06:53 -0400

    Add files via upload
    Updated first fragment code

--- app/src/main/java/com/example/pinak/test2/FirstFragment.kt
+++ app/src/main/java/com/example/pinak/test2/FirstFragment.kt
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 
 /**
@@ -17,11 +18,22 @@ import android.view.ViewGroup
  * Use the [FirstFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-public class FirstFragment : Fragment() {
+public class FirstFragment : Fragment(),WordCallBackListener<String> {
+
+    override fun onSuccess(`object`: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onFailure(e: Exception) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+
+
+
         return inflater!!.inflate(R.layout.fragment_first, container, false)
     }
 
